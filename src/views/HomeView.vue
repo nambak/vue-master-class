31833ffe48a6b3ef12edd7c21c2af2003f260164
남성룡 <nambak@namseonglyong-ui-MacBookPro.local>
2024-08-09 16:17:38 +0900

    add router link

---
+++ src/views/HomeView.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div>
+    <h1>Home Page</h1>
+    <RouterLink to="/projects">Projects</RouterLink>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
