b06a2967bedccc3845f8ad2a8ace0c33bf3ef3e3
남성룡 <nambak@namseonglyong-ui-MacBookPro.local>
2024-08-20 13:29:52 +0900

    add tasks page

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -1,6 +1,8 @@
 import { createClient } from '@supabase/supabase-js'
+import type { Database } from '../../database/types'
 
-export const supabase = createClient(
+export const supabase = createClient<Database>(
   import.meta.env.VITE_SUPABASE_URL,
   import.meta.env.VITE_SUPABASE_KEY
 )
+
--- src/pages/projects/index.vue
+++ src/pages/projects/index.vue
@@ -1,12 +1,32 @@
 <script setup lang="ts">
+import { supabase } from '@/lib/supabaseClient'
+import { ref } from 'vue'
+import type { Tables } from '../../../database/types'
 
+const projects = ref<Tables<'projects'>[] | null>(null)
+
+;(async () => {
+  const { data, error } = await supabase.from('projects').select()
+
+  if (error) {
+    console.error(error)
+  }
+  projects.value = data
+
+  console.log('projects: ', projects)
+})()
 </script>
 
 <template>
-    <div>
-      <h1>Projects Page</h1>
-      <RouterLink to="/public">Home</RouterLink>
-    </div>
+  <div>
+    <h1>Projects Page</h1>
+    <RouterLink to="/">Go to home</RouterLink>
+    <ul>
+      <li v-for="project in projects" :key="project.id">
+        {{ project.name }}
+      </li>
+    </ul>
+  </div>
 </template>
 
 <style scoped>
